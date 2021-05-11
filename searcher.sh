bed3b76bbea42e28217da0fc6458ae5f7e1a2a2c
Hi Im Chernbog! <84031833+Chernbog@users.noreply.github.com>
2021-05-11 12:05:45 -0400

    Add important files

---
+++ searcher.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo 'choose the year you would like to search in: '
+read Year  
+
+echo 'choose the month of the year you would like to search in: '
+read Month
+
+echo 'choose the broodstock you would like to search in: '
+read Broodstock
+
+# echo "You chose the year $Year the Month of $Month the Broodstock type $Broodstock"
+
+csvcut -c collection_year,collection_month,broodstock,collection_date,shell_height_mm All-ORS-Data-All-Time.csv | csvgrep -c collection_year -r "$Year" | csvgrep -c collection_month -r "$Month" | csvgrep -c broodstock -r "$Broodstock" | csvsort -c collection_date -r | csvlook
+
+echo -n "would you like the average shell height of that month to(y/n)?: " 
+read answer
+
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+	csvcut -c collection_year,collection_month,broodstock,shell_height_mm All-ORS-Data-All-Time.csv | csvgrep -c collection_year -r "$Year" | csvgrep -c collection_month -r "$Month" | csvgrep -c broodstock -r "$Broodstock" | csvcut -c shell_height_mm | csvstat
+else
+    echo No
+fi
